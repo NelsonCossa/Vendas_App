a97093c2d9a700afe607442f1a96419275d976c6
Nelson Cossa <ncossa754@gmail.com>
2025-06-03 04:15:00 +0200

    Criada a userService

---
+++ src/main/java/com/nelson/app/services/UserService.java
@@ -0,0 +1,51 @@
+package com.nelson.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.nelson.app.entities.User;
+import com.nelson.app.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		
+			repository.deleteById(id);
+		
+	}
+	
+	public User update(Long id, User obj) {
+		
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+}
