41bca820ebe5638f9b343b1b8f98402905a517d4
Nelson Cossa <ncossa754@gmail.com>
2025-06-03 04:21:34 +0200

    Modicada a classe UserService, adicionando o tratamento de excepcoes e implementacao do metodo findById

--- src/main/java/com/nelson/app/services/UserService.java
+++ src/main/java/com/nelson/app/services/UserService.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import com.nelson.app.entities.User;
 import com.nelson.app.repositories.UserRepository;
+import com.nelson.app.services.exceptions.DatabaseException;
+import com.nelson.app.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,24 +25,33 @@ public class UserService {
 		return repository.findAll();
 	}
 	
-	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
 	
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 	
 	public void delete(Long id) {
-		
+		try {
 			repository.deleteById(id);
-		
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
-		
+		try {
 			User entity = repository.getOne(id);
 			updateData(entity, obj);
 			return repository.save(entity);
-			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
