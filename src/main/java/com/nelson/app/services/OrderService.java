c863cd7ef702fd704aaf52c2201fbecb01d4490a
Nelson Cossa <ncossa754@gmail.com>
2025-06-03 04:06:12 +0200

    Criada a camada de service, a OrderService

---
+++ src/main/java/com/nelson/app/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.nelson.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelson.app.entities.Order;
+import com.nelson.app.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
