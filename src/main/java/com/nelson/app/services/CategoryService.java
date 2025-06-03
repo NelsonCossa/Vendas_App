df5e31803171ff992c5fa8ae1777bfee079aba5f
Nelson Cossa <ncossa754@gmail.com>
2025-06-03 04:09:03 +0200

    Criada a CategoryService

---
+++ src/main/java/com/nelson/app/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.nelson.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelson.app.entities.Category;
+import com.nelson.app.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
